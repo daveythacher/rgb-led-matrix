4b5f8db25742c3a06d312cefde4c9b978159c9d2
David Thacher <davidethacher@gmail.com>
2025-06-08 14:54:01 -0400

    updating files

--- rgb-led-matrix/include/Drawer/Drawer.h
+++ rgb-led-matrix/include/Drawer/Drawer.h
@@ -8,11 +8,11 @@ namespace rgb_matrix {
     // Pure interface for drawing
     class Drawer {
         public:
-            virtual void SetPixel(uint16_t x, uint16_t y, uint8_t red, uint8_t green, uint8_t blue) = 0;
-            virtual void SetPixel(cord_t cord, pixel_t pixel) { SetPixel(cord.x, cord.y, pixel.red, pixel.green, pixel.blue); }
+            virtual void SetPixel(cord_t cord, pixel_t pixel) = 0;
             virtual cord_t get_size() = 0;
             virtual void set_brightness(uint8_t brightness) = 0;
             virtual void map_wavelength(uint8_t color, Color index, uint16_t value) = 0;
+            virtual void map_dot_correct(cord_t pos, uint8_t color, Color index, float value) = 0;
     };
 }
 #endif
